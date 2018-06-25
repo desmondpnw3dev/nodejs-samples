9f9ce2c031721b6b7b214ca071ce6fd7304d93a8
Desmond <desmond@pnw3dev.com>
2018-06-24 18:54:03 -0700

    project structures

---
+++ examples/project-structure-monolith/client/app/entity-name/scripts/routes.js
@@ -0,0 +1,23 @@
+
+/**
+ * @file routes.js
+ * @description Module/Route index - Initializes modules and routes
+ * @author Desmond G. Jones <desmond@pnw3dev.com>
+ * @license ISC
+ * @copyright 2018, Inc
+ */
+
+// REQUIRE DEPENDENCIES
+// ...
+
+// INITIALIZE MODULES
+// require('./module-name')();
+
+/**
+ * @param {object} app - express app instance - middleware
+ */
+module.exports = ({app}) => {
+    // app.get('/',(req,res) => {})
+};
+
+console.info('Service Modules Initialized and Ready!');
