3afbf380b1355786839431be8487a3e854e6d2c2
Desmond G. Jones <desmond@pnw3dev.com>
2018-06-24 19:06:38 -0700

    comments

--- examples/fs/lib/fs-read.test.js
+++ examples/fs/lib/fs-read.test.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 const fs = require('fs');
 // Create a group of tests about Arrays
 describe('fs', function() {
-  // Within our Array group, Create a group of tests for indexOf
+  // Within our fs group, Create a group of tests for readFile
   describe('#readFile()', function() {
     // A string explanation of what we're testing
     it('should read file without error', function(done) {
